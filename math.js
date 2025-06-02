0daad26954f837bbcd4544a1ca6e8a6529d08226
Kabir058 <riduanulkabir01@gmail.com>
2025-06-02 08:19:05 +0600

    uses of math in javascript

---
+++ math.js
@@ -0,0 +1,19 @@
+// let value = 5.6;
+// console.log(Math.round(value))
+
+let values =2,5,9;
+console.log(Math.min(array))
+
+console.log(Math.max(array))
+
+
+| `Math.round(x)`     | Rounds to nearest integer     | `Math.round(4.7)`   | `5`              |
+| `Math.floor(x)`     | Rounds **down**               | `Math.floor(4.9)`   | `4`              |
+| `Math.ceil(x)`      | Rounds **up**                 | `Math.ceil(4.1)`    | `5`              |
+| `Math.max(...nums)` | Largest number                | `Math.max(2, 4, 9)` | `9`              |
+| `Math.min(...nums)` | Smallest number               | `Math.min(2, 4, 9)` | `2`              |
+| `Math.random()`     | Random number between 0 and 1 | `Math.random()`     | e.g., `0.379...` |
+| `Math.pow(x, y)`    | Power                         | `Math.pow(2, 3)`    | `8`              |
+| `Math.sqrt(x)`      | Square root                   | `Math.sqrt(25)`     | `5`              |
+| `Math.abs(x)`       | Absolute value                | `Math.abs(-7)`      | `7`              |
+| `Math.PI`           | Value of π                    | `Math.PI`           | `3.14159...`     |
